0ac122ac15c8686ccdb6d7632ad0a088201442df
Annia Forney <anniaforney@gmail.com>
2019-04-19 20:35:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,11 @@ def square_array(array)
   array.each 
 end 
 
-def square_array(array) 
-  puts ""
+def square_array(array) do 
+  elements = [0,1,2]
+  square_array(elements)
+  new elements = [3,4,5,6]
+  square_array(new elements)
 end
 
 def square_array(array) 
